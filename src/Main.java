e8006bbb7f5cebb81da92d3cccf62d964e60f7f1
Omar <luizbr507z@gmail.com>
2021-02-25 18:38:33 +0100

    remove printlns

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("test");
         File example = new File("src/resources/TODO.todo");
-        System.out.println(example.getName());
         ScanFile scanner = new ScanFile();
         scanner.read(example);
     }
